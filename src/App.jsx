bf7ecf728b6100ddbf856e2cfb3efb481ade4bb1
Andrey <And.konobeev.ser@gmail.com>
2020-04-28 00:26:36 +0300

    new project

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import './App.css';
-import DatePicker from './components/DatePicker'
-import DateAndrey from './components/DateAndrey'
+
 
 const App = () => {
 
     return  (
         <div>
-        
-            <DatePicker/>
-            <DateAndrey/>
         </div>
     );
 }
